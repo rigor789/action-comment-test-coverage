1f32af21ded6942ad02a119702a7e4525ce2bcd4
rigor789 <rigor789@gmail.com>
2020-03-20 19:29:56 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Comment Test Coverage
+
+A GitHub action to comment on a PR with a simple test coverage summary and a link to the full-coverage report.
+
---
+++ index.js
@@ -0,0 +1,51 @@
+const { inspect } = require("util");
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require('fs');
+
+async function run() {
+  try {
+    const inputs = {
+      token: process.env.GITHUB_TOKEN,
+      path: core.getInput('coveragePath'),
+      coverageURL: core.getInput('coverageURL')
+    };
+
+    const {
+      payload: { pull_request: pullRequest, repository }
+    } = github.context;
+
+    if (!pullRequest) {
+      core.error('This action only works on pull_request events');
+      return;
+    }
+
+    const { number: issueNumber } = pullRequest;
+    const { full_name: repoFullName } = repository;
+    const [owner, repo] = repoFullName.split('/');
+
+    const octokit = new github.GitHub(inputs.token);
+
+    const data = fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/${inputs.path}`, 'utf8');
+    const json = JSON.parse(data);
+
+    const outro = coverageURL ? `\nFull Coverage Report: ${coverageURL}` : ''
+    const coverage = `==== **Coverage** ====
+Statements: ${json.total.statements.pct}% ( ${json.total.statements.covered}/${json.total.statements.total} )
+Branches  : ${json.total.branches.pct}%   ( ${json.total.branches.covered}  /${json.total.branches.total} )
+Functions : ${json.total.functions.pct}%  ( ${json.total.functions.covered} /${json.total.functions.total} )
+Lines     : ${json.total.lines.pct}%      ( ${json.total.lines.covered}     /${json.total.lines.total} )`
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: eval('`' + coverage + outro + '`')
+    });
+  } catch (error) {
+    core.debug(inspect(error));
+    core.setFailed(error.message);
+  }
+}
+
+run();
